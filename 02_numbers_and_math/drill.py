41a8af144aa2c7ba1ea827507de5d95e44559c81
Sharkabo <ian.c.tw@icloud.com>
2026-01-26 22:15:52 +0800

    Finish Unit 02

--- 02_numbers_and_math/drill.py
+++ 02_numbers_and_math/drill.py
@@ -2,15 +2,16 @@
 # ----------------------------------
 
 # Drill 1: Create a variable x = 10 and y = 5
-
-
+x = 10
+y = 5
 # Drill 2: Create a variable z that is x plus y, then print z
-
-
+z = x + y
+print(z)
 # Drill 3: Print x multiplied by y
-
+print(x*y)
 
 # Drill 4: Create a float variable price = 9.99
-
+price = 9.99
 
 # Drill 5: Print price divided by 3
+print(price/3)
