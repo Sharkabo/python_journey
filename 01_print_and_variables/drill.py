a23eaaa6c4c5ec437239552f8f076507221c6fc3
Sharkabo <ian.c.tw@icloud.com>
2026-01-26 21:17:37 +0800

    Finish Unit 01, update README for the missing part

--- 01_print_and_variables/drill.py
+++ 01_print_and_variables/drill.py
@@ -2,15 +2,17 @@
 # ------------------------------------
 
 # Drill 1: Print "Hello" to the screen
-
+print('Hello')
 
 # Drill 2: Print the number 123 to the screen
-
+print('123')
 
 # Drill 3: Create a variable named 'pet' and set it to "Dog"
-
+pet = 'Dog'
 
 # Drill 4: Print the variable 'pet'
-
+print(pet)
 
 # Drill 5: Create a variable named 'city' with value "Taipei", then print "I live in " + city
+city = 'Taipei'
+print(f'I live in {city}')
