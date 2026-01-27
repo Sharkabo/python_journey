4730d35918e8b421a8809db2ba086c9d9fec353c
Sharkabo <ian.c.tw@icloud.com>
2026-01-27 22:20:46 +0800

    Finish Unit 05

--- 05_lists_and_containers/drill.py
+++ 05_lists_and_containers/drill.py
@@ -2,15 +2,16 @@
 # -----------------------
 
 # Drill 1: Create a list named 'colors' with "Red", "Blue", "Green"
-
+colors = ["Red", "Blue", "Green"]
 
 # Drill 2: Print the first item in 'colors' (Remember index 0!)
-
+print(colors[0])
 
 # Drill 3: Print the last item in 'colors'
-
+print(colors[2])
 
 # Drill 4: Add "Yellow" to the end of the list using .append()
-
+colors.append("Yellow")
 
 # Drill 5: Print the whole list to see the changes
+print(colors)
