b0ba00b01327fc3d8c4e56be95475a0e65e34d6c
Sharkabo <ian.c.tw@icloud.com>
2026-01-27 10:50:37 +0800

    Finished Unit 04

--- 04_decisions_if_else/answer.py
+++ 04_decisions_if_else/answer.py
@@ -1,6 +1,12 @@
 # Complete your task here (refer to task.md)
 
 # Goal 1: Ask for temperature input
-
+temp = float(input('Please input a temperature in Celsius'))
 
 # Goal 2: Check temperature and print advice
+if temp > 30:
+    print("It's hot, drink water")
+elif 30>= temp >=15:
+    print("Nice weather")
+else:
+    print('Bring a jacket')
