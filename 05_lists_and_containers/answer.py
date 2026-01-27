93654777388fa00b967d79fd630bd1be72df61cf
Sharkabo <ian.c.tw@icloud.com>
2026-01-27 22:20:46 +0800

    Finished Unit 05

--- 05_lists_and_containers/answer.py
+++ 05_lists_and_containers/answer.py
@@ -1,12 +1,14 @@
 # Complete your task here (refer to task.md)
 
 # Goal 1: Create todo_list with initial items
-
+todo_list = ["Buy Milk", "Walk Dog"]
 
 # Goal 2: Ask user for new task
-
+new_task = input('Enter a new task')
 
 # Goal 3: Add new task to list
-
+todo_list.append(new_task)
 
 # Goal 4: Print first and last task
+print(todo_list[0])
+print(todo_list[2])
