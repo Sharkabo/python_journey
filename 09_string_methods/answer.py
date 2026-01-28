331a9f87efd2f4680b5e5cff05fa718a822a1faa
Sharkabo <ian.c.tw@icloud.com>
2026-01-28 23:40:08 +0800

    Finished Unit 09

--- 09_string_methods/answer.py
+++ 09_string_methods/answer.py
@@ -1,10 +1,10 @@
 # Complete your task here (refer to task.md)
 
 # 1. Clean User Input
-
-
+source_name = input('Please input username...')
+name = source_name.strip().replace(' ','').title()
 # 2. Create a Username
-
+username = name.lower().replace(' ',"_")
 
 # 3. Display Result
-
+print(f'Welcome, {name}! Your username is: {username}')
