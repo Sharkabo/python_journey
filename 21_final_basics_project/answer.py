4fe9de2e943092eecc2d851e8547dfebb21b67a6
Sharkabo <ian.c.tw@icloud.com>
2026-01-29 23:36:05 +0800

    Finished Unit 21 which is the entire basic training

--- 21_final_basics_project/answer.py
+++ 21_final_basics_project/answer.py
@@ -1,6 +1,12 @@
 # Complete your task here (refer to task.md)
 
 # Goal 1: Open journal.txt in append mode
-
+with open('journal.txt', 'a') as f:
+    while True:
+        user_input = input("Write entry (or type 'exit' to quit): ")
+        if user_input == 'exit':
+            break
+        else:
+            f.write(user_input + "\n")
 
 # Goal 2: Start loop, ask for input, check exit, and write to file
