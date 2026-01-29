5e144969875dcf3e9736056540aed93eff0be192
Sharkabo <ian.c.tw@icloud.com>
2026-01-29 15:59:04 +0800

    Finished Unit 10

--- 10_boolean_logic/answer.py
+++ 10_boolean_logic/answer.py
@@ -1,10 +1,20 @@
 # Complete your task here (refer to task.md)
 
 # 1. Get User Information
-
-
+user_age = int(input('Please input your age...'))
+membership_ans = input('Do you have a membership card? (yes/no):').strip().lower()
+if membership_ans == 'yes':
+    have_membership = True
+elif membership_ans == 'no':
+    have_membership = False
+else:
+    print('Your answer doesn\'t meet the requirements. Try again...')
 # 2. Check Access Rules
-
-
+if user_age >= 18 and have_membership == True:
+    print('Access Granted! Welcome!')
+elif user_age >= 65 and have_membership == False:
+    print('Senior citizens get free entry')
+else:
+    print('Access Denied. Requirements not met')
 # 3. Display Result
 
