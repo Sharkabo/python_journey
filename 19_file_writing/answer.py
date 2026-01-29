e5f40f0e5320286b644b6b0254d02e3d9204c96d
Sharkabo <ian.c.tw@icloud.com>
2026-01-29 22:59:36 +0800

    Finished Unit 19

--- 19_file_writing/answer.py
+++ 19_file_writing/answer.py
@@ -1,9 +1,11 @@
 # Complete your task here (refer to task.md)
 
 # Goal 1: Ask for favorite food
-
-
+user_input = input('What is your favorite food?')
 # Goal 2: Open food.txt and write answer to file
-
-
+with open('food.txt', 'w') as f:
+    f.write(user_input)
 # Goal 3: Check file exists (manual)
+with open('food.txt', 'r') as f:
+    content = f.read()
+    print(content)
--- 19_file_writing/drill.py
+++ 19_file_writing/drill.py
@@ -2,6 +2,17 @@
 # ------------------------------
 
 # Drill 1: Open "log.txt" in write mode with `with open("log.txt", "w") as f:` and write "Log Entry 1" using f.write()
-
+with open('log.txt', 'w') as f:
+    f.write('You did modify the file.')
+    
+with open('log.txt', 'r') as f:
+    content = f.read()
+    print(content)
 
 # Drill 2: Open "log.txt" in append mode with `with open("log.txt", "a") as f:` and write "Log Entry 2" to see it add to the end
+with open('log.txt', 'a') as f:
+    f.write('Log Entry 2')
+
+with open('log.txt', 'r') as f:
+    content = f.read()
+    print(content)
