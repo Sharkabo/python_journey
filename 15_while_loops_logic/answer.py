d90084ee849e32671df044bf1c33a22bd3fb66b3
Sharkabo <ian.c.tw@icloud.com>
2026-01-29 21:57:51 +0800

    Finished Unit 15

--- 15_while_loops_logic/answer.py
+++ 15_while_loops_logic/answer.py
@@ -1,9 +1,11 @@
 # Complete your task here (refer to task.md)
 
 # Goal 1: Create energy variable
-
+energy = 5
 
 # Goal 2: Create while loop to print energy and decrease
-
-
+while energy > 0:
+    print(f'Energy level: {energy}')
+    energy = energy - 1
 # Goal 3: Print Game Over
+print('Game Over')
--- 15_while_loops_logic/drill.py
+++ 15_while_loops_logic/drill.py
@@ -2,15 +2,19 @@
 # -----------------------------
 
 # Drill 1: Create a variable x = 0
-
+x = 0
 
 # Drill 2: Write a while loop: while x < 5, print x, then increase x by 1
-
-
+while x < 5:
+    print(x)
+    x = x +1
 # Drill 3: Write a while loop: while x > 0, print x, then decrease x by 1
-
-
+while x > 0:
+    print(x)
+    x = x - 1
 # Drill 4: Create variable running = True. Write `while running:`, print "Hi" and set running = False
-
-
+running = True
+while running:
+    print('Hi')
+    running = False
 # Drill 5: (Mental Drill) What happens if you forget to change x inside the loop? (Answer: Infinite Loop!)
