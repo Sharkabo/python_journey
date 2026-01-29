1be4de376e33c98c108a78f1ffad276a7e55cdcb
Sharkabo <ian.c.tw@icloud.com>
2026-01-29 22:02:03 +0800

    Finished Unit 16

--- 16_libraries_imports/drill.py
+++ 16_libraries_imports/drill.py
@@ -2,15 +2,17 @@
 # ---------------------------
 
 # Drill 1: Import the random library
-
+import random
 
 # Drill 2: Use random.randint(1, 10) to print a random number
-
-
+num = random.randint(1, 10)
+print(num)
 # Drill 3: Import the math library
-
+import math
 
 # Drill 4: Print math.sqrt(25)
-
+print(math.sqrt(25))
 
 # Drill 5: Import datetime and print datetime.date.today()
+import datetime
+print(datetime.date.today())
