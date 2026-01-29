c1ef9edd3ab433be7a6cc699838a2a71768d71a0
Sharkabo <ian.c.tw@icloud.com>
2026-01-29 19:50:46 +0800

    Finished Unit 13

--- 13_dictionaries_key_value/answer.py
+++ 13_dictionaries_key_value/answer.py
@@ -1,9 +1,11 @@
 # Complete your task here (refer to task.md)
 
 # Goal 1: Create book dictionary
-
+book = {'titile': 'Harry Poter', 'author': 'J.K. Rowling', 'year': 1997}
 
 # Goal 2: Print sentence using values
-
+print(f'{book["titile"]} was written by {book["author"]}.')
 
 # Goal 3: Update year and check
+book['year'] = 2000
+print(book['year'])
