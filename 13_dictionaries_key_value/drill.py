9cbf4d9c967fe79171d00b62161f345f70826957
Sharkabo <ian.c.tw@icloud.com>
2026-01-29 19:50:46 +0800

    Finished Unit 13

--- 13_dictionaries_key_value/drill.py
+++ 13_dictionaries_key_value/drill.py
@@ -2,15 +2,16 @@
 # ------------------------------
 
 # Drill 1: Create a dictionary 'person' with "name": "Ian" and "age": 20
-
+person = {'name': 'Ian', 'age': 20}
 
 # Drill 2: Print the value stored under the key "name"
-
+print(person['name'])
 
 # Drill 3: Change the "age" key to 21
-
+person['age'] = 21
 
 # Drill 4: Add a new key "city" with value "Taipei"
-
+person['City'] = 'Taipei'
 
 # Drill 5: Print the whole dictionary
+print(person)
