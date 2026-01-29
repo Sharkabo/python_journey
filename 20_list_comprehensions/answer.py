c02d75624c9c9e58b3c1f6aaed5daa9e96c1d3fe
Sharkabo <ian.c.tw@icloud.com>
2026-01-29 23:26:51 +0800

    Finished Unit 20

--- 20_list_comprehensions/answer.py
+++ 20_list_comprehensions/answer.py
@@ -1,13 +1,16 @@
 # Complete your task here (refer to task.md)
 
 # 1. Temperature Converter
-
-
+temp_C = [0, 10, 20, 30, 40]
+temp_F = [C * 9/5 + 32 for C in temp_C]
+print(temp_F)
 # 2. Filter Passing Grades
-
-
+scores = [45, 67, 89, 52, 78, 91, 34, 88]
+passing_grade = [score for score in scores if score >= 60]
+print(passing_grade)
 # 3. Clean User Input
-
-
+source_inputs = ["  hello  ", "  world  ", "  python  "]
+cleaned_inputs = [input.strip() for input in source_inputs]
+print(cleaned_inputs)
 # 4. Display All Results
-
+print(f'Fahrenheit: {temp_F}\nPassing Grades: {passing_grade}\nCleaned Input: {cleaned_inputs}')
--- 20_list_comprehensions/drill.py
+++ 20_list_comprehensions/drill.py
@@ -2,16 +2,24 @@
 # ---------------------------------------
 
 # Drill 1: Create a list of squares from 1 to 5 using list comprehension: [1, 4, 9, 16, 25]
-
-
+numbers = [1, 2, 3, 4, 5]
+squares = [num * num for num in numbers]
+print(squares)
 # Drill 2: Create a list of numbers from 0 to 9, but only include even numbers using list comprehension
-
+nums = []
+for i in range(10):
+    if i % 2 == 0:
+        nums.append(i)
+print(nums)
 
 # Drill 3: Given names = ["alice", "bob", "charlie"], create a new list with all names in uppercase
-
-
+names = ["alice", "bob", "charlie"]
+upper_names = [name.upper() for name in names]
 # Drill 4: Given numbers = [1, 2, 3, 4, 5, 6], create a list of numbers greater than 3
-
-
+numbers = [1, 2, 3, 4, 5, 6]
+big_num = [num for num in numbers if num > 3]
+print(big_num)
 # Drill 5: Create a list of the lengths of words in ["cat", "dog", "elephant"] using list comprehension
-
+animals = ["cat", "dog", "elephant"]
+len_animals = [len(name) for name in animals]
+print(len_animals)
