bd0eaa04cab8e88dedfe84277af4cb70ad7836a3
Sharkabo <ian.c.tw@icloud.com>
2026-01-29 22:32:18 +0800

    Finished Unit 17

--- 17_error_handling_try_except/answer.py
+++ 17_error_handling_try_except/answer.py
@@ -1,3 +1,9 @@
 # Complete your task here (refer to task.md)
 
 # Goal 1: Try block with input and doubling, Except block for ValueError with error message
+try:
+    user_input = int(input('Enter number:'))
+    output = user_input * 2
+    print(output)
+except:
+    print('Please enter digits only.')
