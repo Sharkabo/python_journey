7cf731b2f8562424e7f2be91ca26cd5ec9bdc622
Sharkabo <ian.c.tw@icloud.com>
2026-01-29 22:02:03 +0800

    Finished Unit 16

--- 16_libraries_imports/answer.py
+++ 16_libraries_imports/answer.py
@@ -1,9 +1,11 @@
 # Complete your task here (refer to task.md)
 
 # Goal 1: Import random library
-
+import random
 
 # Goal 2: Generate and print random number
-
-
+num = random.randint(1, 100)
+print(f'The magic number is: {num}')
 # Goal 3 (Bonus): Import datetime and print year
+import datetime
+print(datetime.date.today().year)
