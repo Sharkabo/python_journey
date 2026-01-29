dc6a85900fc61509c4897d3ff70e2dc1304c80e5
Sharkabo <ian.c.tw@icloud.com>
2026-01-29 22:32:18 +0800

    Finished Unit 17

--- 17_error_handling_try_except/drill.py
+++ 17_error_handling_try_except/drill.py
@@ -1,10 +1,26 @@
 # Drills - Unit 17: Try/Except
 # ----------------------------
 
-# Drill 1: Write a try block that prints int("Hello"), and an except ValueError that prints "Cannot convert text to number"
+# Drill 1: Write a try/except to catch a conversion error
+#   - In the try: attempt to convert text to a number with int("Hello") - this will fail!
+#   - In the except ValueError: print "Cannot convert text to number"
+try:
+    int('Hello')
+except ValueError:
+    print('Cannot convert text to number')
 
-
-# Drill 2: Write a try block that does 10 / 0, and an except ZeroDivisionError that prints "Cannot divide by zero"
-
-
-# Drill 3: Write a simple try/except that catches ANY error (except Exception:) and prints "Oops"
+# Drill 2: Write a try/except to catch a division by zero error
+#   - In the try: attempt 10 / 0 - this will cause an error!
+#   - In the except ZeroDivisionError: print "Cannot divide by zero"
+try:
+    num = 10 / 0
+    print(num)
+except:
+    print('Cannot divide by zero')
+# Drill 3: Write a try/except that catches ANY type of error
+#   - In the try: write code that causes an error (int("abc") or 1/0 or anything that breaks)
+#   - In the except Exception: print "Oops, something went wrong!"
+try:
+    int("abc")
+except:
+    print('Oops, something went wrong!')
