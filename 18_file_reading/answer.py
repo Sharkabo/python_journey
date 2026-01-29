95b1b980d1fe7a2ebf40f9dce49f7139ce60026e
Sharkabo <ian.c.tw@icloud.com>
2026-01-29 22:43:33 +0800

    Finished Unit 18

--- 18_file_reading/answer.py
+++ 18_file_reading/answer.py
@@ -1,3 +1,6 @@
 # Complete your task here (refer to task.md)
 
 # Goal 1: Open hello.txt in read mode, read content, and print it
+with open('hello.txt', 'r') as f:
+    content = f.read()
+    print(content)
--- 18_file_reading/drill.py
+++ 18_file_reading/drill.py
@@ -1,7 +1,14 @@
 # Drills - Unit 18: File Reading
 # ------------------------------
 
-# Drill 1: (Assume test.txt exists) Open "test.txt" with `with open("test.txt", "r") as f:`, read content with f.read(), and print it
-
-
+# Drill 1: (Assume hello.txt exists) Open "hello.txt" with `with open("hello.txt", "r") as f:`, read content with f.read(), and print it
+with open('hello.txt', 'r') as f:
+    content = f.read()
+    print(content) 
 # Drill 2: Write a try block that tries to open a non-existent file, and an except block that prints "File not found"
+try:
+    with open('test.txt', 'r') as f:
+        content = f.read()
+        print(content)
+except:
+    print('Check the file path or the working directory')
