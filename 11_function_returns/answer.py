2730a5968252b4e98e8de69722c0c84e2636f946
Sharkabo <ian.c.tw@icloud.com>
2026-01-29 16:36:57 +0800

    Finished Unit 11

--- 11_function_returns/answer.py
+++ 11_function_returns/answer.py
@@ -1,10 +1,22 @@
 # Complete your task here (refer to task.md)
 
 # 1. Create Math Functions
-
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    return a / b 
 
 # 2. Get User Input
-
-
+a = int(input('Please input first number:'))
+b = int(input('Please input second number:'))
 # 3. Perform Calculations
+sum = add(a, b)
+difference = subtract(a, b)
+product = multiply(a, b)
+quotient = divide(a, b)
 
+print(f'{a} + {b} = {sum}\n{a} - {b} = {difference}\n{a} * {b} = {product}\n{a} / {b} = {quotient}')
